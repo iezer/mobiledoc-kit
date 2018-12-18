14c6e710ff8224aa088607485fe215f6cd922c0b
Isaac Ezer <isaac.ezer@gmail.com>
2018-12-18 11:12:13 +0100

    Wip use rollup in brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 /* jshint node:true */
 
 var broccoli = require("broccoli");
+var concatenate = require("broccoli-concat");
 var Watcher = require("broccoli-sane-watcher");
 var Funnel = require("broccoli-funnel");
 var builder = require("broccoli-multi-builder");
@@ -18,34 +19,91 @@ var vendoredModules = [
 
 var cssFiles = new Funnel("src/css", { destDir: "css" });
 
-var packageName = require("./package.json").name;
-
-var buildOptions = {
-  libDirName: "src/js",
-  vendoredModules: vendoredModules,
-  packageName: packageName
-};
-
 var testTree = testTreeBuilder.build({ libDirName: "src" });
 testTree = jquery.build(testTree, "/tests/jquery");
 testTree = new BroccoliLiveReload(testTree, { target: "index.html" });
 
 var testBuilder = new broccoli.Builder(testTree);
 
-function replaceVersion(tree) {
-  var version = require("./package.json").version;
-  return replace(tree, {
-    files: [ "**/*.js" ],
-    pattern: { match: /##VERSION##/g, replacement: version }
+const USE_ROLLUP = true;
+
+if (!USE_ROLLUP) {
+  var packageName = require("./package.json").name;
+
+  var buildOptions = {
+    libDirName: "src/js",
+    vendoredModules: vendoredModules,
+    packageName: packageName
+  };
+
+  function replaceVersion(tree) {
+    var version = require("./package.json").version;
+    return replace(tree, {
+      files: ["**/*.js"],
+      pattern: { match: /##VERSION##/g, replacement: version }
+    });
+  }
+
+  module.exports = mergeTrees([
+    replaceVersion(builder.build("amd", buildOptions)),
+    replaceVersion(builder.build("global", buildOptions)),
+    replaceVersion(builder.build("commonjs", buildOptions)),
+    cssFiles,
+    testTree,
+    demoTree()
+  ]);
+} else {
+  const Rollup = require("broccoli-rollup");
+  const babel = require("rollup-plugin-babel");
+  const resolve = require("rollup-plugin-node-resolve");
+  const commonjs = require("rollup-plugin-commonjs");
+  const importAlias = require("rollup-plugin-import-alias");
+
+  let jsTrees = [
+    ["global", "iife", "Mobiledoc"]
+    // ["commonjs", "cjs", "Mobiledoc"],
+    //["amd", "amd", "mobiledoc-kit"],
+    //["es6", "es", "mobiledoc-kit"]
+    // ["umd", "umd", "mobiledoc-kit"]
+  ].map(function(configuration) {
+    const outputDir = configuration[0];
+    const outputFormat = configuration[1];
+    const outputName = configuration[2];
+
+    return new Rollup("src/js", {
+      inputFiles: ["**/*.js"],
+      annotation: "JS Transformation",
+      rollup: {
+        input: "index.js",
+        output: {
+          name: outputName,
+          file: outputDir + "/mobiledoc-kit.js",
+          format: outputFormat,
+          amd: {
+            id: "mobiledoc-kit"
+          },
+          sourcemap: true
+        },
+        plugins: [
+          babel({
+            exclude: "node_modules/**"
+          }),
+          resolve({
+            jsnext: true,
+            main: true
+          }),
+          commonjs(),
+          importAlias({
+            Paths: {
+              "mobiledoc-kit": "src/js"
+            },
+            Extensions: ["js"]
+          })
+        ],
+        external: ["mobiledoc-kit"]
+      }
+    });
   });
+
+  module.exports = mergeTrees(jsTrees.concat([cssFiles, testTree, demoTree()]));
 }
-
-module.exports = mergeTrees([
-  replaceVersion(builder.build("amd", buildOptions)),
-  replaceVersion(builder.build("global", buildOptions)),
-  replaceVersion(builder.build("commonjs", buildOptions)),
-  cssFiles,
-  testTree,
-  demoTree()
-]);
-
